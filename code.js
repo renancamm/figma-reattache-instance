67b0722cffb37284048be63e2d53e4f73048762c
Александр <forjob159@gmail.com>
2019-09-11 15:28:43 +0300

    Add overrides applier

--- code.js
+++ code.js
@@ -1,38 +1,135 @@
-function reattachInstance() {
-    let skippedCount = 0;
-    let processedCount = 0;
-    let originalInstances = {};
-    if (figma.currentPage.selection.length == 0) {
-        return "Please, select a frame first";
+/**
+ * According to Figma API docs overrides support:
+ * - colours;
+ * - text styles;
+ * - and effects.
+ *
+ * @see https://help.figma.com/article/306-using-instances#override
+ */
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const effectsProps = ['effectStyleId', 'effects'];
+const colorProps = [
+    'backgroundStyleId',
+    'backgrounds',
+    'opacity',
+    'fills',
+    'strokes',
+    'strokeWeight',
+    'strokeAlign',
+    'strokeCap',
+    'strokeJoin',
+    'dashPattern',
+    'fillStyleId',
+    'strokeStyleId',
+];
+const textStyleProps = [
+    'fillStyleId',
+    'fills',
+    'textAlignHorizontal',
+    'textAlignVertical',
+    'textAutoResize',
+    'paragraphIndent',
+    'paragraphSpacing',
+    'textStyleId',
+    'fontSize',
+    'fontName',
+    'textCase',
+    'textDecoration',
+    'letterSpacing',
+    'lineHeight',
+];
+function clone(val) {
+    return JSON.parse(JSON.stringify(val));
+}
+function cloneProps(fromNode, toNode, propsList) {
+    propsList.forEach(prop => {
+        if (!fromNode[prop])
+            return;
+        toNode[prop] = clone(fromNode[prop]);
+    });
+}
+function extractFontName(node) {
+    const fontsList = [];
+    if (node.type === 'TEXT') {
+        fontsList.push(node.fontName);
     }
-    const clonedSelection = Object.assign([], figma.currentPage.selection);
-    for (let index in clonedSelection) {
-        let frame = clonedSelection[index];
-        let instanceReference;
-        if (frame.type !== "FRAME") {
+    if (node.children && node.children.length) {
+        node.children.forEach(child => {
+            fontsList.push(...extractFontName(child));
+        });
+    }
+    return fontsList;
+}
+function loadFonts(node) {
+    return Promise.all(extractFontName(node).map(fontName => {
+        return figma.loadFontAsync(fontName);
+    }));
+}
+function applyOverrides(frame, instance) {
+    cloneProps(frame, instance, effectsProps);
+    cloneProps(frame, instance, colorProps);
+    if (instance.type === 'TEXT' && frame.type === 'TEXT') {
+        return cloneProps(frame, instance, textStyleProps);
+    }
+    if (!frame.children.length) {
+        return;
+    }
+    frame.children.forEach((frameChild, index) => {
+        const instanceChild = instance.children[index];
+        applyOverrides(frameChild, instanceChild);
+    });
+}
+function reattachInstance() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let skippedCount = 0;
+        let processedCount = 0;
+        let originalInstances = {};
+        if (figma.currentPage.selection.length == 0) {
+            return "Please, select a frame first";
+        }
+        const clonedSelection = Object.assign([], figma.currentPage.selection);
+        for (let index in clonedSelection) {
+            let frame = clonedSelection[index];
+            let instanceReference;
+            if (frame.type !== "FRAME") {
+                skippedCount += 1;
+                continue;
+            }
+            if (!(frame.name in originalInstances)) {
+                instanceReference = figma.currentPage.findOne(node => node.type === "INSTANCE" && node.name == frame.name);
+                originalInstances[frame.name] = instanceReference;
+            }
+            else {
+                instanceReference = originalInstances[frame.name];
+            }
+            if (instanceReference != null) {
+                let instanceClone = instanceReference.masterComponent.createInstance();
+                frame.parent.appendChild(instanceClone);
+                instanceClone.x = frame.x;
+                instanceClone.y = frame.y;
+                instanceClone.resize(frame.width, frame.height);
+                // need to load fonts first, otherwise it won't apply font styles
+                yield loadFonts(frame);
+                yield loadFonts(instanceClone);
+                applyOverrides(frame, instanceClone);
+                frame.remove();
+                processedCount += 1;
+                continue;
+            }
             skippedCount += 1;
             continue;
         }
-        if (!(frame.name in originalInstances)) {
-            instanceReference = figma.currentPage.findOne(node => node.type === "INSTANCE" && node.name == frame.name);
-            originalInstances[frame.name] = instanceReference;
-        }
-        else {
-            instanceReference = originalInstances[frame.name];
-        }
-        if (instanceReference != null) {
-            let instanceClone = instanceReference.masterComponent.createInstance();
-            frame.parent.appendChild(instanceClone);
-            instanceClone.x = frame.x;
-            instanceClone.y = frame.y;
-            instanceClone.resize(frame.width, frame.height);
-            frame.remove();
-            processedCount += 1;
-            continue;
-        }
-        skippedCount += 1;
-        continue;
-    }
-    return `${processedCount} processed, ${skippedCount} skipped`;
+        return `${processedCount} processed, ${skippedCount} skipped`;
+    });
 }
-figma.closePlugin(reattachInstance());
+(() => __awaiter(this, void 0, void 0, function* () {
+    figma.closePlugin(yield reattachInstance());
+}))();
--- code.ts
+++ code.ts
@@ -1,4 +1,99 @@
-function reattachInstance() {
+/**
+ * According to Figma API docs overrides support:
+ * - colours;
+ * - text styles;
+ * - and effects.
+ *
+ * @see https://help.figma.com/article/306-using-instances#override
+ */
+
+const effectsProps = ['effectStyleId', 'effects'];
+const colorProps = [
+    'backgroundStyleId',
+    'backgrounds',
+    'opacity',
+    'fills',
+    'strokes',
+    'strokeWeight',
+    'strokeAlign',
+    'strokeCap',
+    'strokeJoin',
+    'dashPattern',
+    'fillStyleId',
+    'strokeStyleId',
+];
+const textStyleProps = [
+    'fillStyleId',
+    'fills',
+    'textAlignHorizontal',
+    'textAlignVertical',
+    'textAutoResize',
+    'paragraphIndent',
+    'paragraphSpacing',
+    'textStyleId',
+    'fontSize',
+    'fontName',
+    'textCase',
+    'textDecoration',
+    'letterSpacing',
+    'lineHeight',
+];
+
+
+function clone(val) {
+    return JSON.parse(JSON.stringify(val));
+}
+
+function cloneProps(fromNode, toNode, propsList) {
+    propsList.forEach(prop => {
+        if (!fromNode[prop]) return;
+        toNode[prop] = clone(fromNode[prop]);
+    });
+}
+
+
+function extractFontName(node) {
+    const fontsList = [];
+
+    if (node.type === 'TEXT') {
+        fontsList.push(node.fontName);
+    }
+
+    if (node.children && node.children.length) {
+        node.children.forEach(child => {
+            fontsList.push(...extractFontName(child));
+        });
+    }
+
+    return fontsList;
+}
+
+function loadFonts(node) {
+    return Promise.all(extractFontName(node).map(fontName => {
+        return figma.loadFontAsync(fontName);
+    }));
+}
+
+function applyOverrides(frame, instance) {
+    cloneProps(frame, instance, effectsProps);
+    cloneProps(frame, instance, colorProps);
+
+    if (instance.type === 'TEXT' && frame.type === 'TEXT') {
+        return cloneProps(frame, instance, textStyleProps);
+    }
+
+    if (!frame.children.length) {
+        return;
+    }
+
+    frame.children.forEach((frameChild, index) => {
+        const instanceChild = instance.children[index];
+        applyOverrides(frameChild, instanceChild);
+    });
+}
+
+
+async function reattachInstance() {
     let skippedCount = 0;
     let processedCount = 0;
     let originalInstances = {};
@@ -31,6 +126,12 @@ function reattachInstance() {
             instanceClone.x = frame.x;
             instanceClone.y = frame.y;
             instanceClone.resize(frame.width, frame.height);
+
+            // need to load fonts first, otherwise it won't apply font styles
+            await loadFonts(frame);
+            await loadFonts(instanceClone);
+            applyOverrides(frame, instanceClone);
+
             frame.remove();
             processedCount += 1;
             continue;
@@ -42,4 +143,6 @@ function reattachInstance() {
     return `${processedCount} processed, ${skippedCount} skipped`;
 }
 
-figma.closePlugin(reattachInstance());
+(async () => {
+    figma.closePlugin(await reattachInstance());
+})()
